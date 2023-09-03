083a668597a860a4aa4e73d60f0466f595059cc8
adidev18 <alagar.18@proton.me>
2023-09-03 20:32:39 +0530

    added-packs-plugs

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "rose-pine-main",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,24 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
    { import = "astrocommunity.colorscheme.rose-pine", enabled = true },
+   { import = "astrocommunity.pack.rust"},
+   { import = "astrocommunity.colorscheme.mini-base16"},
+   { import = "astrocommunity.pack.typescript-all-in-one"},
+   { import = "astrocommunity.pack.python"},
+   { import = "astrocommunity.pack.markdown"},
+   { import = "astrocommunity.pack.bash"},
+   { import = "astrocommunity.pack.go"},
+   { import = "astrocommunity.pack.html-css"},
+   { import = "astrocommunity.debugging.nvim-dap-repl-highlights"},
+   { import = "astrocommunity.debugging.nvim-dap-virtual-text"},
+   { import = "astrocommunity.debugging.nvim-bqf"},
+   { import = "astrocommunity.code-runner.overseer-nvim"},
+   { import = "astrocommunity.test.nvim-coverage"},
+   { import = "astrocommunity.test.neotest"},
+   { import = "astrocommunity.bars-and-lines/heirline-vscode-winbar"},
+   { import = "astrocommunity.diagnostics.lsp_lines-nvim"},
+
+
+
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
